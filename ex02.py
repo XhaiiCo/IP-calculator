3c2e9583362bc6cd15a37fb4065611b6f72f9731
Leclercq David <davidleclercq2002@gmail.com>
2022-09-23 09:06:06 +0200

    Continue ex02

--- ex02.py
+++ ex02.py
@@ -1,16 +1,10 @@
 import ex01
+import helper as h
 # Exercice 02
 # Si le masque correspond au masque de la classfull alors masque de reseau sinon masque de sous reseau
 
 # Donne l'adresse du sous réseau en binaire
 # params: ip en binaire et masque en binaire
-
-def ex02(ip, masque):
-    classe = ex01.findClasse(ip)
-    if not classe:
-        return classe #TODO
-
-print(ex02("222.232.232.3", "0.0.0.0"))
 
 
 def SR(ip, masque):
@@ -45,3 +39,16 @@ def broadcast(ip, masque):
 
     return result
 
+
+def ex02(ip, masque):
+    classe = ex01.findClasse(ip)
+    if classe and classe["masque"] == masque:
+        return classe["masque"]
+
+    return h.toPointer(SR(h.toBinary(ip), h.toBinary(masque)))
+
+
+ip = "130.234.234.234"
+masque = "255.12.0.0"
+print(ex02(ip, masque))
+print(h.toPointer(broadcast(h.toBinary(ip), h.toBinary(masque))))
