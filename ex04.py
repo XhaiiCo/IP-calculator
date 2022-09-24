3e06932730161c2174e5d7485d74096873a24f16
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2022-09-24 16:13:57 +0200

    Optimization of ex 4

--- ex04.py
+++ ex04.py
@@ -1,28 +1,15 @@
 import math
 import helper as h
-        
-def isIpInNetwork(addressIp1, maskAddress1, addressIp2) :
-    if not h.verifMasque(maskAddress1) : return False
-    if not (h.verifIP(addressIp1) and h.verifIP(addressIp2)) : return False
-
-    nb1InMask = h.toBinary(maskAddress1).count("1") # Compte le nombre de 1 pour comparer les ips
-    nb1InMask += math.trunc(nb1InMask / 8) # Compte le nomre de .
-    addressIp1Binary = h.toBinary(addressIp1)  # Binaire
-    addressIp2Binary = h.toBinary(addressIp2)  # Binaire
-
-    if addressIp1Binary[0:nb1InMask] == addressIp2Binary[0:nb1InMask] :
-        return True
-
-    return False
+import ex03
 
 def compareAddressAndNetwork(ip1, mask1, ip2, mask2) :
     str = ""
-    if (isIpInNetwork(ip1, mask1, ip2)) :
+    if (ex03.isIpInNetwork(ip1, mask1, ip2)) :
         str += "La première IP considère que la deuxième IP est dans son réseau\n"
     else :
         str += "La première IP ne considère pas que la deuxième IP est dans son réseau\n"
 
-    if (isIpInNetwork(ip2, mask2, ip1)) :
+    if (ex03.isIpInNetwork(ip2, mask2, ip1)) :
         str += "La deuxième IP considère que la première IP est dans son réseau"
     else :
         str += "La deuxième IP ne considère pas que la première IP est dans son réseau"
@@ -31,10 +18,14 @@ def compareAddressAndNetwork(ip1, mask1, ip2, mask2) :
 
     
 
-ip1 =   "192.168.128.0"
+ip1 =   "192.168.192.0"
 mask1 = "255.255.128.0"
+# print(h.toBinary(ip1))
+# print(h.toBinary(mask1))
 
-ip2 =   "192.168.192.0"
+ip2 =   "192.168.128.0"
 mask2 = "255.255.192.0"
+# print(h.toBinary(ip2))
+# print(h.toBinary(mask2))
 
 # print(compareAddressAndNetwork(ip1, mask1, ip2, mask2))
