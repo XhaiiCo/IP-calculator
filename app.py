d3f7272be9c72c1457af8248ca059f761227d1a8
David Leclercq <davidleclercq2002@gmail.com>
2022-09-24 18:38:12 +0200

    Connect auth works

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
-# import auth 
+import auth 
 from tkinter import *
 from tkinter import ttk
 
-# if auth.login(input("login: "), input("password: ")): 
-#     print("loged")
-# else:
-#     print("Erreur")
 
-def logIn(*args):
-    try:
-        log = str(login.get())
-        passw = pwd.get()
-        print(log)
-        print(passw)
+
+def logIn():
+    log = str(login.get())
+    passw = str(pwd.get())
+    if auth.login(log, passw):
+         print("loged")
+    else:
+         print("Erreur")
 
         #Tester le mdp et le login
-    except ValueError:
-        pass
 
 root = Tk()
-root.title("Log")
+root.title("Connexion")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -31,7 +27,7 @@ login_entry = ttk.Entry(mainframe, width=10, textvariable=login)
 login_entry.grid(column=2, row=1, sticky=(W, E))
 
 pwd = StringVar()
-pwd_entry = ttk.Entry(mainframe, width=10, textvariable=pwd)
+pwd_entry = ttk.Entry(mainframe, width=10, textvariable=pwd, show='*')
 pwd_entry.grid(column=2, row=2, sticky=(W, E))
 
 ttk.Button(mainframe, text="Log in", command=logIn).grid(column=3, row=3, sticky=W)
