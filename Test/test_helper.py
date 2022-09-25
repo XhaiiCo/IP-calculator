5d473adcb84f3814a7d438d5be0e66a3baa60a27
David Leclercq <davidleclercq2002@gmail.com>
2022-09-25 20:18:44 +0200

    Start unit test

---
+++ Test/test_helper.py
@@ -0,0 +1,27 @@
+import unittest
+
+from util.helper import verifIP, verifMasque
+
+
+class Test(unittest.TestCase):
+    def test_verif_ip(self):
+        self.assertEqual(verifIP("0.0.0.0"), True)
+        self.assertEqual(verifIP(""), False)
+        self.assertEqual(verifIP("0."), False)
+        self.assertEqual(verifIP("0.0"), False)
+        self.assertEqual(verifIP("0.0.0"), False)
+        self.assertEqual(verifIP("0.0.0.0"), True)
+        self.assertEqual(verifIP("001.001.001.001"), True)
+        self.assertEqual(verifIP("255.255.255.255"), True)
+        self.assertEqual(verifIP("256.434.0.0"), False)
+        self.assertEqual(verifIP("-23.-23.0.0"), False)
+        self.assertEqual(verifIP("..."), False)
+
+    def test_verif_masque(self):
+        self.assertEqual(verifMasque("255.255.255.0"), True)
+        self.assertEqual(verifMasque("255.255..0"), False)
+        self.assertEqual(verifMasque("223.0.255.0"), False)
+        self.assertEqual(verifMasque("255.-255.0.0"), False)
+        self.assertEqual(verifMasque("255.255.0.0"), True)
+        self.assertEqual(verifMasque("256.255.0.0"), False)
+
