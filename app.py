d9f70c68ee4ba95944ab7201a575c354eceabd81
David Leclercq <davidleclercq2002@gmail.com>
2022-09-25 17:42:24 +0200

    Center the login form

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import auth
 from tkinter import *
 from tkinter import ttk
 
-
-
-def logIn(*args):
+def logIn():
     log = str(login.get())
     passw = str(pwd.get())
     if auth.login(log, passw):
@@ -12,30 +10,29 @@ def logIn(*args):
     else:
          print("Erreur")
 
-        #Tester le mdp et le login
-
+# fenetre
 root = Tk()
 root.title("Connexion")
+root.geometry("600x400")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+loginframe = ttk.Frame(root, padding="3 3 12 12")
+loginframe.pack(expand=YES)
 
 login = StringVar()
-login_entry = ttk.Entry(mainframe, width=10, textvariable=login)
+login_entry = ttk.Entry(loginframe, width=30, textvariable=login)
 login_entry.grid(column=2, row=1, sticky=(W, E))
 
 pwd = StringVar()
-pwd_entry = ttk.Entry(mainframe, width=10, textvariable=pwd, show='*')
-pwd_entry.grid(column=2, row=2, sticky=(W, E))
+pwd_entry = ttk.Entry(loginframe, width=30, textvariable=pwd, show='*')
+pwd_entry.grid(column=2, row=2, sticky=(W, E), pady=3)
 
-ttk.Button(mainframe, text="Log in", command=logIn).grid(column=3, row=3, sticky=W)
+ttk.Button(loginframe, text="Log in", command=logIn).grid(column=2, row=3, sticky=W, pady=3)
 
-ttk.Label(mainframe, text="Login").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="Password").grid(column=3, row=2, sticky=W)
+ttk.Label(loginframe, text="Login").grid(column=1, row=1, sticky=W, pady=3)
+ttk.Label(loginframe, text="Password").grid(column=1, row=2, sticky=W)
 
 login_entry.focus()
 root.bind("<Return>", logIn)
 
+# Affichage
 root.mainloop()
