afd993f55d958a22bf7d28b37a2428872b636a4c
VicGuillaume <67600842+VicGuillaume@users.noreply.github.com>
2022-09-27 12:35:09 +0200

    Added error string

--- view/login.py
+++ view/login.py
@@ -8,30 +8,39 @@ from view import app
 def view(window):
     window.title("Connexion")
 
+    # TODO minwidth window
     loginframe = ttk.Frame(window, padding="3 3 12 12")
     loginframe.pack(expand=YES)
 
-    login = StringVar()
-    login_entry = ttk.Entry(loginframe, width=30, textvariable=login)
+    lblLogin = StringVar()
+    login_entry = ttk.Entry(loginframe, width=30, textvariable=lblLogin)
     login_entry.grid(column=2, row=1, sticky=W)
 
-    pwd = StringVar()
-    pwd_entry = ttk.Entry(loginframe, width=30, textvariable=pwd, show='*')
+    lblPwd = StringVar()
+    pwd_entry = ttk.Entry(loginframe, width=30, textvariable=lblPwd, show='*')
     pwd_entry.grid(column=2, row=2, sticky=W, pady=3)
 
-    ttk.Button(loginframe, text="Log in", command=lambda: logIn(login.get(), pwd.get(), window)).grid(column=2, row=3,
-                                                                                                      sticky=E, pady=3)
-
     ttk.Label(loginframe, text="Login").grid(column=1, row=1, sticky=W, pady=5)
-    ttk.Label(loginframe, text="Password").grid(column=1, row=2, sticky=W, padx=(0, 10))
+    ttk.Label(loginframe, text="Password").grid(column=1, row=2, sticky=W, padx=(0, 15))
+
+    ttk.Button(loginframe, text="Log in", command=lambda: logIn(window, loginframe, lblLogin.get(), lblPwd.get(),
+                                                                login_entry, pwd_entry)).grid(column=2, row=3,
+                                                                                              sticky=E, pady=3)
 
     login_entry.focus()
 
 
-def logIn(login, password, window):
-    log = str(login)
-    passw = str(password)
+def logIn(window, loginframe, lblLogin, lblPassword, login_entry, pwd_entry):
+    log = str(lblLogin)
+    passw = str(lblPassword)
     if auth.login(log, passw):
         app.main(window)
     else:
-        print("Erreur")
+        ttk.Label(loginframe, text="Mauvais login ou mot de passe !", foreground='red')\
+            .grid(column=1, columnspan=2, row=3, sticky=W, padx=(0, 10))
+
+        login_entry.delete(0, END)
+        login_entry.insert(0, "")
+        pwd_entry.delete(0, END)
+        pwd_entry.insert(0, "")
+
