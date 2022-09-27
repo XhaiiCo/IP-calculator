1d2727ad523a70df1dec5edcf5b9f9a6bdf9a5fa
Leclercq David <davidleclercq2002@gmail.com>
2022-09-27 18:33:39 +0200

    Make new test for ex 5

--- test/test_ex_05.py
+++ test/test_ex_05.py
@@ -23,6 +23,9 @@ class Test(unittest.TestCase):
         self.assertEqual(nb_max_hote("255.255.128.0", 24), result_message["possible"].format(1022))
         self.assertEqual(nb_max_hote("255.255.255.0", 64), result_message["possible"].format(2))
         self.assertEqual(nb_max_hote("255.255.255.0", 65), result_message["impossible"])
+        self.assertEqual(nb_max_hote("255.255.255.192", 1), result_message["possible"].format(62))
+        self.assertEqual(nb_max_hote("255.255.255.252", 1), result_message["possible"].format(2))
+        self.assertEqual(nb_max_hote("255.255.255.0", 0), result_message["erreur"]["<=0"])
 
     def test_nb_max_sr(self):
         result_message = message.tab["5"]["hotes"]
@@ -31,6 +34,12 @@ class Test(unittest.TestCase):
         self.assertEqual(nb_max_sr("255.255.0.0", 350), result_message["possible"].format(127))
         self.assertEqual(nb_max_sr("255.255.255.0", 100), result_message["possible"].format(1))
         self.assertEqual(nb_max_sr("255.255.255.0", 200), result_message["impossible"])
+        self.assertEqual(nb_max_sr("255.255.255.0", 1), result_message["possible"].format(63))
+        self.assertEqual(nb_max_sr("255.255.255.192", 1), result_message["possible"].format(15))
+        self.assertEqual(nb_max_sr("255.255.255.252", 1), result_message["impossible"])
+        self.assertEqual(nb_max_sr("255.255.255.0", 0), result_message["erreur"]["<=0"])
+
+
 
 
 
