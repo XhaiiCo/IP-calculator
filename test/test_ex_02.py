bf61c8d4e25a06169152932100759e9bb520e38c
Leclercq David <davidleclercq2002@gmail.com>
2022-09-27 15:02:13 +0200

    Make test for ex 2

--- test/test_ex_02.py
+++ test/test_ex_02.py
@@ -1,5 +1,6 @@
 import unittest
-from model.ex02 import valid_masque_for_classe
+from model.ex02 import valid_masque_for_classe, ex02, broadcast, reseau
+from util import message
 
 
 class Test(unittest.TestCase):
@@ -10,3 +11,31 @@ class Test(unittest.TestCase):
         self.assertEqual(valid_masque_for_classe("255.252.0.0", "B"), False)
         self.assertEqual(valid_masque_for_classe("255.255.255.0", "C"), True)
         self.assertEqual(valid_masque_for_classe("255.255.252.0", "C"), False)
+
+
+    def test_ex02(self):
+        result_message = message.tab["2"]
+        # Invalid ip
+        self.assertEqual(ex02("0.0.0.0", "255.0.0.0"), [result_message["IP invalid"]])
+        self.assertEqual(ex02("127.0.0.0", "255.0.0.0"), [result_message["IP invalid"]])
+        self.assertEqual(ex02("224.0.0.0", "255.0.0.0"), [result_message["IP invalid"]])
+        self.assertEqual(ex02("240.0.0.0", "255.0.0.0"), [result_message["IP invalid"]])
+
+        # Invalid masque
+        self.assertEqual(ex02("1.0.0.0", "192.0.0.0"), [result_message["masque invalid"]])
+        self.assertEqual(ex02("128.0.0.0", "255.252.0.0"), [result_message["masque invalid"]])
+        self.assertEqual(ex02("192.0.0.0", "255.255.252.0"), [result_message["masque invalid"]])
+
+        #broadcast
+        self.assertEqual(broadcast("192.0.0.0", "255.255.255.0"), "192.0.0.255")
+        self.assertEqual(broadcast("128.0.0.0", "255.255.0.0"), "128.0.255.255")
+        self.assertEqual(broadcast("1.0.0.0", "255.0.0.0"), "1.255.255.255")
+
+        #Reseau
+        self.assertEqual(reseau("192.0.0.0", "255.255.255.0"), "192.0.0.0")
+        self.assertEqual(reseau("128.0.0.0", "255.255.0.0"), "128.0.0.0")
+        self.assertEqual(reseau("1.0.0.0", "255.0.0.0"), "1.0.0.0")
+        self.assertEqual(reseau("129.32.23.1", "255.255.192.0"), "129.32.0.0")
+        self.assertEqual(reseau("20.234.231.20", "255.252.0.0"), "20.232.0.0")
+
+
