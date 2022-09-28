e8fa7db28c33da5e023da7fb5da5c726ac80d43f
Leclercq David <davidleclercq2002@gmail.com>
2022-09-28 12:04:14 +0200

    Start error gestion in view

--- view/main_window.py
+++ view/main_window.py
@@ -1,7 +1,9 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 
 from model import ex01, ex02, ex03, ex04, ex05
+from util import message
+from util.helper import verifIP, verifMasque
 
 
 def main_window(window):
@@ -25,7 +27,7 @@ def main_window(window):
 
     # "Déterminer la classe d'une IP"
     ttk.Button(leftMenuContainer, text="Exercice 1",
-               command=lambda: display(1, rightInputContainer, rightOutputContainer))\
+               command=lambda: display(1, rightInputContainer, rightOutputContainer)) \
         .grid(column=1, row=1, sticky=E, pady=3)
 
     # "Déterminer l'adresse réseau + broadcast"
@@ -50,6 +52,7 @@ def main_window(window):
 
 
 def display(exo, frameInput, frameOutput):
+    result_message = message.tab
     # Reset les frames
     for widget in frameInput.winfo_children():
         widget.destroy()
@@ -62,38 +65,57 @@ def display(exo, frameInput, frameOutput):
 
     if exo == 1:
         ttk.Label(rightInputContainer, text="Entrez l'IP").grid(column=1, row=1, sticky=W)
-        ip = StringVar()
-        ip_entry = ttk.Entry(rightInputContainer, width=30, textvariable=ip)
+        ip_value = StringVar()
+        ip_entry = ttk.Entry(rightInputContainer, width=30, textvariable=ip_value)
         ip_entry.grid(column=1, row=2, sticky=W)
 
         ttk.Button(rightInputContainer, text="Text",
-                   command=lambda: displayOutput(ex01.ex01(ip.get()), frameOutput))\
+                   command=lambda: submit()) \
             .grid(column=1, row=3, sticky=E, pady=3)
+
+        def submit():
+            ip = ip_value.get()
+            if not verifIP(ip):
+                display_output([result_message["error"]["ip"]],frameOutput)
+                return
+            display_output(ex01.ex01(ip), frameOutput)
 
     elif exo == 2:
         ttk.Label(rightInputContainer, text="Entrez l'IP").grid(column=1, row=1, sticky=W)
-        ip = StringVar()
-        ip_entry = ttk.Entry(rightInputContainer, width=30, textvariable=ip)
+        ip_value = StringVar()
+        ip_entry = ttk.Entry(rightInputContainer, width=30, textvariable=ip_value)
         ip_entry.grid(column=1, row=2, sticky=W)
 
         ttk.Label(rightInputContainer, text="Entrez le masque").grid(column=1, row=3, sticky=W, pady=(5, 0))
-        masque = StringVar()
-        masque_entry = ttk.Entry(rightInputContainer, width=30, textvariable=masque)
+        masque_value = StringVar()
+        masque_entry = ttk.Entry(rightInputContainer, width=30, textvariable=masque_value)
         masque_entry.grid(column=1, row=4, sticky=W)
 
         ttk.Button(rightInputContainer, text="Text",
-                   command=lambda: displayOutput(ex02.ex02(ip.get(), masque.get()), frameOutput))\
+                   command=lambda: submit()) \
             .grid(column=1, row=5, sticky=E, pady=(7, 0))
+
+        def submit():
+            ip = ip_value.get()
+            masque = masque_value.get()
+            if not verifIP(ip):
+                display_output([result_message["error"]["ip"]], frameOutput)
+                return
+            if not verifMasque(masque):
+                display_output([result_message["error"]["masque"]], frameOutput)
+                return
+
+            display_output(ex02.ex02(ip, masque), frameOutput)
 
     elif exo == 3:
         ttk.Label(rightInputContainer, text="Entrez l'adresse IP").grid(column=1, row=1, sticky=W)
-        ip = StringVar()
-        ip_entry = ttk.Entry(rightInputContainer, width=30, textvariable=ip)
+        ip_value = StringVar()
+        ip_entry = ttk.Entry(rightInputContainer, width=30, textvariable=ip_value)
         ip_entry.grid(column=1, row=2, sticky=W)
 
         ttk.Label(rightInputContainer, text="Entrez le masque").grid(column=1, row=3, sticky=W, pady=(5, 0))
-        masque = StringVar()
-        masque_entry = ttk.Entry(rightInputContainer, width=30, textvariable=masque)
+        masque_value = StringVar()
+        masque_entry = ttk.Entry(rightInputContainer, width=30, textvariable=masque_value)
         masque_entry.grid(column=1, row=4, sticky=W)
 
         ttk.Label(rightInputContainer, text="Entrez l'adresse du réseau").grid(column=1, row=5, sticky=W, pady=(5, 0))
@@ -102,8 +124,9 @@ def display(exo, frameInput, frameOutput):
         reseau_entry.grid(column=1, row=6, sticky=W)
 
         ttk.Button(rightInputContainer, text="Text",
-                   command=lambda: displayOutput(ex03.ex03(ip.get(), masque.get(), reseau.get()), frameOutput))\
+                   command=lambda: display_output(ex03.ex03(ip_value.get(), masque_value.get(), reseau.get()), frameOutput)) \
             .grid(column=1, row=7, sticky=E, pady=(7, 0))
+
 
     elif exo == 4:
         ttk.Label(rightInputContainer, text="Entrez la première adresse IP").grid(column=1, row=1, sticky=W)
@@ -111,7 +134,8 @@ def display(exo, frameInput, frameOutput):
         ip1_entry = ttk.Entry(rightInputContainer, width=40, textvariable=ip1)
         ip1_entry.grid(column=1, row=2, sticky=W)
 
-        ttk.Label(rightInputContainer, text="Entrez le masque de la première adresse IP").grid(column=1, row=3, sticky=W, pady=(5, 0))
+        ttk.Label(rightInputContainer, text="Entrez le masque de la première adresse IP").grid(column=1, row=3,
+                                                                                               sticky=W, pady=(5, 0))
         masque1 = StringVar()
         masque1_entry = ttk.Entry(rightInputContainer, width=40, textvariable=masque1)
         masque1_entry.grid(column=1, row=4, sticky=W)
@@ -121,13 +145,16 @@ def display(exo, frameInput, frameOutput):
         ip2_entry = ttk.Entry(rightInputContainer, width=40, textvariable=ip2)
         ip2_entry.grid(column=1, row=6, sticky=W)
 
-        ttk.Label(rightInputContainer, text="Entrez le masque de la deuxième  adresse IP").grid(column=1, row=7, sticky=W, pady=(5, 0))
+        ttk.Label(rightInputContainer, text="Entrez le masque de la deuxième  adresse IP").grid(column=1, row=7,
+                                                                                                sticky=W, pady=(5, 0))
         masque2 = StringVar()
         masque2_entry = ttk.Entry(rightInputContainer, width=40, textvariable=masque2)
         masque2_entry.grid(column=1, row=8, sticky=W)
 
         ttk.Button(rightInputContainer, text="Text",
-                   command=lambda: displayOutput(ex04.compare_address_and_network(ip1.get(), masque1.get(), ip2.get(), masque2.get()), frameOutput))\
+                   command=lambda: display_output(
+                       ex04.compare_address_and_network(ip1.get(), masque1.get(), ip2.get(), masque2.get()),
+                       frameOutput)) \
             .grid(column=1, row=9, sticky=E, pady=(7, 0))
 
     elif exo == 5:
@@ -136,33 +163,35 @@ def display(exo, frameInput, frameOutput):
         nbSR_entry = ttk.Entry(rightInputContainer, width=40, textvariable=nbSR)
         nbSR_entry.grid(column=1, row=2, sticky=W)
 
-        ttk.Label(rightInputContainer, text="Entrez un nombre d'hôte par réseau").grid(column=1, row=3, sticky=W, pady=(5, 0))
+        ttk.Label(rightInputContainer, text="Entrez un nombre d'hôte par réseau").grid(column=1, row=3, sticky=W,
+                                                                                       pady=(5, 0))
         nbHParSR = StringVar()
         nbHParSR_entry = ttk.Entry(rightInputContainer, width=40, textvariable=nbHParSR)
         nbHParSR_entry.grid(column=1, row=4, sticky=W)
 
         ttk.Label(rightInputContainer, text="Entrez une adresse IP").grid(column=1, row=5, sticky=W)
-        ip = StringVar()
-        ip_entry = ttk.Entry(rightInputContainer, width=40, textvariable=ip)
+        ip_value = StringVar()
+        ip_entry = ttk.Entry(rightInputContainer, width=40, textvariable=ip_value)
         ip_entry.grid(column=1, row=6, sticky=W)
 
         ttk.Label(rightInputContainer, text="Entrez le masque de départ").grid(column=1, row=7, sticky=W, pady=(5, 0))
-        masque = StringVar()
-        masque_entry = ttk.Entry(rightInputContainer, width=40, textvariable=masque)
+        masque_value = StringVar()
+        masque_entry = ttk.Entry(rightInputContainer, width=40, textvariable=masque_value)
         masque_entry.grid(column=1, row=8, sticky=W)
 
         ttk.Button(rightInputContainer, text="Text",
-                   command=lambda: displayOutput(ex05.ex05(nbSR.get(), nbHParSR.get(), ip.get(), masque.get()), frameOutput))\
+                   command=lambda: display_output(ex05.ex05(nbSR.get(), nbHParSR.get(), ip_value.get(), masque_value.get()),
+                                                  frameOutput)) \
             .grid(column=1, row=9, sticky=E, pady=(7, 0))
 
 
-def displayOutput(exoReturn, frameOutput):
-    for widget in frameOutput.winfo_children():
+def display_output(exo_return, frame_output):
+    for widget in frame_output.winfo_children():
         widget.destroy()
 
     # Container droit output
-    rightOutputContainer = Frame(frameOutput)
-    rightOutputContainer.pack(expand=True)
+    right_output_container = Frame(frame_output)
+    right_output_container.pack(expand=True)
 
-    for i, output in enumerate(exoReturn):
-        ttk.Label(rightOutputContainer, text=output).grid(column=1, row=i, sticky=W, padx=5)
+    for i, output in enumerate(exo_return):
+        ttk.Label(right_output_container, text=output).grid(column=1, row=i, sticky=W, padx=5)
